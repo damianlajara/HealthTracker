c1e124a16350966f39bd2bc58464c7acc867b7b3
ZalmanB <zalmanblau@gmail.com>
2015-11-17 13:11:33 -0500

    changed to production mode

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,4 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-RAILS_ENV=['production'] rails server
+RAILS_ENV=['production']
